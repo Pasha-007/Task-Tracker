78ea1c43b84b43c506f875983b40b48c8796ec89
Pasha-007 <m.muntahaa@gmail.com>
2023-01-31 15:53:39 +0600

    Initial commit

---
+++ src/main/java/kg/edu/alatoo/Task/Tracker/controllers/GreatingController.java
@@ -0,0 +1,15 @@
+package kg.edu.alatoo.Task.Tracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreatingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name= "name", required = false, defaultValue = "Noname") String name, Model model){
+        model.addAttribute( "name", name);
+        return "greeting";
+    }
+}
---
+++ src/test/java/kg/edu/alatoo/Task/Tracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.edu.alatoo.Task.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
